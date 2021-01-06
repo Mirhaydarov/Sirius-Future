6400b8e038bf028393e23eb03644e09894017e3e
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2021-01-06 16:44:39 +0500

    refactor: change MainLayout component
    Now it take i child for decide what should be render. As result MainLayout become more reusable.
    Also all blank page now have a navbar for easy switch on different page.

--- pages/finance.tsx
+++ pages/finance.tsx
@@ -3,13 +3,18 @@ import Link from 'next/link'
 
 // Components
 import { Wrap, LinkWrap } from './index'
+import { MainLayout } from '../components/MainLayout'
+import { Navbar } from '../components/Navbar'
 
 // Component
 const Finance = () => (
-  <Wrap>
-    <h1>Финансы</h1>
-    <Link href='/students'><LinkWrap>Вернуться назад</LinkWrap></Link>
-  </Wrap>
+  <MainLayout>
+    <Navbar />
+    <Wrap>
+      <h1>Финансы</h1>
+      <Link href='/students'><LinkWrap>Вернуться назад</LinkWrap></Link>
+    </Wrap>
+  </MainLayout>
 );
 
 export default Finance
--- pages/lessons.tsx
+++ pages/lessons.tsx
@@ -3,13 +3,18 @@ import Link from 'next/link'
 
 // Components
 import { Wrap, LinkWrap } from './index'
+import { MainLayout } from '../components/MainLayout'
+import { Navbar } from '../components/Navbar'
 
 // Component
 const Lessons = () => (
-  <Wrap>
-    <h1>Занятия</h1>
-    <Link href='/students'><LinkWrap>Вернуться назад</LinkWrap></Link>
-  </Wrap>
+  <MainLayout>
+    <Navbar />
+    <Wrap>
+      <h1>Занятия</h1>
+      <Link href='/students'><LinkWrap>Вернуться назад</LinkWrap></Link>
+    </Wrap>
+  </MainLayout>
 );
 
 export default Lessons
